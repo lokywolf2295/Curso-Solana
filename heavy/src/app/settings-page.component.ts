58ea408d40b52980e622846548dd199b5ecf99fb
lokywolf2295 <matiasesek1995@gmail.com>
2024-02-18 21:58:40 -0300

    Update: Refactor settings page component template

--- heavy/src/app/settings-page.component.ts
+++ heavy/src/app/settings-page.component.ts
@@ -1,11 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'heavy-settings-page',
-    template: `
-        <h2>Settings</h2>
-    `,
-    standalone: true,
+  selector: 'heavy-settings-page',
+  template: `
+    <div class="flex justify-center items-center">
+      <section class="px-24 py-32 bg-white bg-opacity-5">
+        <p class="text-center text-3xl">Settings</p>
+      </section>
+    </div>
+  `,
+  standalone: true,
 })
-
 export class SettingsPageComponent {}
